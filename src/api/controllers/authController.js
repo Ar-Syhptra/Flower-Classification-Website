cf8f131f88478f15cc2160a78eac132943810449
Arkansyahputra <theark338@gmail.com>
2025-03-15 23:41:26 +0700

    feat: menambahkan fitur authentication dan authorization

---
+++ src/api/controllers/authController.js
@@ -0,0 +1,31 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.createUser(username, password);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findUsername(username);
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Invalid username or password" });
+    }
+
+    const token = jwt.sign({ id: user.id, username: user.username }, "SECRET_KEY", { expiresIn: "1h" });
+    res.json({ token });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+module.exports = { register, login };
---
+++ src/api/models/User.js
@@ -0,0 +1,20 @@
+const db = require("../config/db");
+const bcrypt = require("bcryptjs");
+
+class User {
+  static async createUser(username, password) {
+    const hashPassword = await bcrypt.hash(password, 10)
+    const result = await db.query(
+      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING id, username",
+      [username, hashPassword]
+    );
+    return result.rows[0];
+  }
+
+  static async findUsername(username) {
+    const result = await db.query("SELECT * FROM users WHERE username = $1", [username])
+    return result.rows[0]
+  }
+}
+
+module.exports = User;
