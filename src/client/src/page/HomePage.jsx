3ac735d4398fbb67484e1c3f31d7fabc531cefaf
sobahus <sobahusn27@gmail.com>
2025-03-17 06:28:26 +0700

    refactor(App): Perubahan struktur code

--- src/client/src/page/HomePage.jsx
+++ src/client/src/page/HomePage.jsx
@@ -1,4 +1,8 @@
 import HeroSection from "../components/HeroSection";
+import HowItWorks from "../components/HowItWorks";
+import AboutFlower from "../components/AboutFlower";
+import DemoSection from "../components/DemoSection";
+import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar";
 import Logo from "../assets/img/flower-logo.svg";
 
@@ -6,10 +10,23 @@ import Logo from "../assets/img/flower-logo.svg";
 function HomePage() {
   return (
     <>
-      <Navbar logo={Logo} tittle="Bloomora" />
+      <Navbar logo={Logo} tittle="FlorAI" />
       <HeroSection
-        header="Pelajari Berbagai Jenis Bunga"
-        description="Temukan dan Identifikasi berbagai jenis bunga."
+        header="Pelajari Berbagai Jenis Bunga dengan FlorAI"
+        description="FlorAI adalah sebuah aplikasi yang dapat membantu Anda mengidentifikasi berbagai jenis bunga. Kami menggunakan teknologi machine learning untuk mempelajari pola-pola dari data bunga yang telah dikumpulkan."
+      />
+      <DemoSection />
+      <HowItWorks />
+      <AboutFlower
+        tittle="Bunga Mawar"
+        description="Gambar di samping adalah bunga mawar, yang termasuk ke dalam famili
+            Rosaceae. Bunga mawar memiliki banyak macam warna, seperti merah,
+            pink, kuning, putih, dan lainnya."
+      />
+      <Footer
+        logo={Logo}
+        tittle="FlorAI"
+        description="lorem ipsum dolor sit amet"
       />
     </>
   );
