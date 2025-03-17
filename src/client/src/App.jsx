cd685bbeb2baddac9b43b4b4bcf451cb7a1886db
sobahus <sobahusn27@gmail.com>
2025-03-17 23:50:23 +0700

    refactor: Memperbarui App.jsx

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,13 +1,25 @@
-// import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./page/HomePage";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar";
+import AboutPage from "./page/AboutPage";
+import ContactPage from "./page/ContactPage";
+import ClassificationPage from "./page/ClassificationPage";
+
 import "./assets/styles/globals.css";
 
 function App() {
-
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/classification" element={<ClassificationPage />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
