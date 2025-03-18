fa9721cb3f4c0488f98d75dab043481259368019
sobahus <sobahusn27@gmail.com>
2025-03-19 06:42:04 +0700

    style: Mengubah style button

--- src/client/src/components/Button.jsx
+++ src/client/src/components/Button.jsx
@@ -1,6 +1,6 @@
 function Button({ name }) {
   return (
-    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl">
+    <button className="btn sm:btn-sm md:btn-md xl:btn-md">
       {name}
     </button>
   );
