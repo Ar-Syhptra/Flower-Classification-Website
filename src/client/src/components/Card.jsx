e80f99d3057cf53cb69a960f5b10625481f54398
sobahus <sobahusn27@gmail.com>
2025-03-22 12:32:01 +0700

    feat: Menambahkan Component baru

---
+++ src/client/src/components/Card.jsx
@@ -0,0 +1,20 @@
+import ButtonModals from "./Button/Button-Modal";
+
+function Card({ cardTittle, cardDescription, cardImage, altImage }) {
+  return (
+    <>
+      <div className="card bg-base-100 w-90 shadow-2xl">
+        <figure>
+          <img src={cardImage} alt={altImage} />
+        </figure>
+        <div className="skeleton h-60 w-full"></div>
+        <div className="card-body items-left text-left">
+          <h2 className="card-title text-2xl">{cardTittle}</h2>
+          <p className="text-md mt-6 text-lg">{cardDescription}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Card;
