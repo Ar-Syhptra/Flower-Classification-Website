0d3e2634acba4f0bd2f02a8754aa2b6e909ea339
Arkansyahputra <theark338@gmail.com>
2025-03-23 00:55:35 +0700

    refactor: mengubah link footer FlorAI

--- src/client/src/components/Footer/Footer.jsx
+++ src/client/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
     <div className="flex justify-center items-center p-10">
       <p className="tracking-tight font-semibold text-xs md:text-sm lg:text-sm">
         Copyright &copy; 2025 - All rights reserved by{" "}
-        <a href="#" className="text-primary font-bold">FlorAI</a> Teams
+        <a href="https://github.com/Ar-Syhptra/Flower-Classification-Website/" className="text-primary font-bold">FlorAI</a> Teams
       </p>
     </div>
   );
