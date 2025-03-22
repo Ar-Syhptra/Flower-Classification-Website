e94bec2f4b19410328d5818b1e807f50f0c1caaa
Arkansyahputra <theark338@gmail.com>
2025-03-22 22:18:25 +0700

    refactor: merapihkan teks footer

--- src/client/src/components/Footer/Footer.jsx
+++ src/client/src/components/Footer/Footer.jsx
@@ -1,16 +1,11 @@
 function Footer() {
   return (
-    <>
-      <div className="flex justify-center items-center p-10">
-        <p className="tracking-tight font-semibold flex text-xs md:text-sm lg:text-sm">
-          Copyright &copy; 2025 - All right reserved by
-          <a href="#" className="text-primary">
-            FlorAI
-          </a>
-          Teams
-        </p>
-      </div>
-    </>
+    <div className="flex justify-center items-center p-10">
+      <p className="tracking-tight font-semibold text-xs md:text-sm lg:text-sm">
+        Copyright &copy; 2025 - All rights reserved by{" "}
+        <a href="#" className="text-primary font-bold">FlorAI</a> Teams
+      </p>
+    </div>
   );
 }
 
