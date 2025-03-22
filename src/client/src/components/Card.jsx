831d68b28d9631d1e7ece2d47a616841810a93b0
sobahus <sobahusn27@gmail.com>
2025-03-22 20:01:16 +0700

    refactor: Perubahan Code

--- src/client/src/components/Card.jsx
+++ src/client/src/components/Card.jsx
@@ -1,18 +1,12 @@
-import ButtonModals from "./Button/Button-Modal";
-
-function Card({ cardTittle, cardDescription, cardImage, altImage }) {
+function Card({ cardTittle, cardDescription }) {
   return (
     <>
-      <div className="card bg-base-100 w-90 shadow-2xl">
-        <figure>
-          <img src={cardImage} alt={altImage} />
-        </figure>
-        <div className="skeleton h-60 w-full"></div>
+      <div className="card w-90 shadow-xl p-4 hover:scale-105 ease-in duration-200 transition-all bg-base-100">
         <div className="card-body items-left text-left">
-          <h2 className="card-title text-xl md:text-xl lg:text-2xl">
+          <h2 className="card-title text-xl md:text-xl lg:text-2xl text-center underline">
             {cardTittle}
           </h2>
-          <p className="text-md md:text-md lg:text-lg mt-6 text-md">
+          <p className="text-md md:text-md lg:text-lg mt-4">
             {cardDescription}
           </p>
         </div>
--- src/client/src/components/Footer/Footer.jsx
+++ src/client/src/components/Footer/Footer.jsx
@@ -1,49 +1,7 @@
-import Logo from "../../assets/img/flower-logo.svg";
-
 function Footer() {
   return (
-    <div className="p-6 ">
-      <div className="flex justify-between px-10 md:px-30 lg:px-40 items-center p-10">
-        <footer className="footer bg-base-100 text-base-content">
-          <nav className="footer footer-horizontal">
-            <div>
-              <h6 className="footer-title">About</h6>
-              <a
-                href="https://github.com/Ar-Syhptra/Flower-Classification-Website"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="link link-hover text-base-800 font-bold flex items-center gap-1"
-              >
-                <img
-                  src="https://img.icons8.com/material/24/github.png"
-                  alt="github"
-                  className="w-6 h-6"
-                />
-                Github
-              </a>
-            </div>
-            <div>
-              <h6 className="footer-title">Contributor</h6>
-
-              <a
-                href=""
-                className="flex gap-1 link link-hover text-base-800 font-bold"
-              >
-                <img
-                  src="https://img.icons8.com/material/24/batch-assign.png"
-                  alt="contributors"
-                  className="w-6 h-6"
-                />
-                Contributors
-              </a>
-            </div>
-          </nav>
-        </footer>
-        <footer className="flex flex-row-reverse">
-          <img className="w-18 h-18" src={Logo} alt="Logo" />
-        </footer>
-      </div>
-      <div className="footer footer-horizontal pt-4 flex justify-center">
+    <>
+      <div className="flex justify-center items-center p-10">
         <p className="tracking-tight font-semibold flex text-xs md:text-sm lg:text-sm">
           Copyright &copy; 2025 - All right reserved by
           <a href="#" className="text-primary">
@@ -52,7 +10,7 @@ function Footer() {
           Teams
         </p>
       </div>
-    </div>
+    </>
   );
 }
 
