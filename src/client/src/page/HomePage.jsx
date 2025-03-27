870166bc124a10221f69d22f64495a2d075e659b
sobahus <sobahusn27@gmail.com>
2025-03-27 09:27:24 +0700

    refactor: Perubahan struktur import file

--- src/client/src/page/HomePage.jsx
+++ src/client/src/page/HomePage.jsx
@@ -1,4 +1,5 @@
 import HeroSection from "../components/Main/HeroSection";
+import WhatIsFlorAI from "../components/Main/WhatIsFlorAI";
 import HowItWorks from "../components/Main/HowItWorks";
 import AboutFlower from "../components/Main/AboutFlower";
 import Navbar from "../components/Navbar/Navbar";
@@ -10,7 +11,7 @@ function HomePage() {
     <>
       <Navbar />
       <HeroSection />
-      <HowItWorks />
+      <WhatIsFlorAI />
       <HowItWorks />
       <AboutFlower />
       <Footer />
