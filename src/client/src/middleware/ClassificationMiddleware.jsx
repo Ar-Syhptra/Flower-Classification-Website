f1a3e754760c37c250035cd08db0e1eb2458f0d7
Arkansyahputra <theark338@gmail.com>
2025-03-30 17:46:48 +0700

    fix: mengubah path middleware

--- src/client/src/middleware/ClassificationMiddleware.jsx
+++ src/client/src/middleware/ClassificationMiddleware.jsx
@@ -1,17 +1,19 @@
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useNavigate, useLocation } from "react-router-dom";
 const ClassificationMiddleware = ({ children }) => {
   const navigate = useNavigate();
+  const location = useLocation(); 
 
   useEffect(() => {
     const token = localStorage.getItem("token");
-    if (!token) {
+    const isAuthPage = location.pathname === "/login" || location.pathname === "/register";
+
+    if (!token && !isAuthPage) {
       navigate("/login");
     }
-  }, [navigate]);
+  }, [navigate, location]);
 
-  return children;
+  return children; 
 };
 
 export default ClassificationMiddleware;
