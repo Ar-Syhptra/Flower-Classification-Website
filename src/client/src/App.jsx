8da6a7e9048a44e441420695a4e67ba5d16528f3
sobahus <sobahusn27@gmail.com>
2025-04-01 20:02:29 +0700

    fix: Perubahan Penempatan router-react-dom
    Perubahan penempatan BrowserRouter di main.jsx ke App.jsx

--- src/client/src/App.jsx
+++ src/client/src/App.jsx
@@ -1,6 +1,6 @@
 import "./assets/styles/globals.css";
 import React, { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoadingSpinner from "./components/Loaders/LoadingSpinner";
 import ClassificationMiddleware from "./middleware/ClassificationMiddleware";
 
@@ -13,7 +13,7 @@ const ClassificationPage = lazy(() => import("./page/ClassificationPage"));
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Suspense fallback={<LoadingSpinner />}>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -21,16 +21,16 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route
-          path="/classification"
-          element={
-            <ClassificationMiddleware>
-              <ClassificationPage />
-            </ClassificationMiddleware>
-          }
-        />
+            path="/classification"
+            element={
+              <ClassificationMiddleware>
+                <ClassificationPage />
+              </ClassificationMiddleware>
+            }
+          />
         </Routes>
       </Suspense>
-    </>
+    </BrowserRouter>
   );
 }
 
