332e30c46612cabaad625d951736dfa948761e15
Arkansyahputra <theark338@gmail.com>
2025-04-15 12:38:12 +0700

    fix: perubahan lokasi environment load model

--- src/api/services/model_loader.py
+++ src/api/services/model_loader.py
@@ -1,3 +1,4 @@
+import os
 import tensorflow as tf
 
 model = None
@@ -5,5 +6,6 @@ model = None
 def load_model():
     global model
     if model is None:
-        model = tf.keras.models.load_model("model-3-a85-c37.h5")
+        model_path = os.path.join(os.path.dirname(__file__), "model-3-a85-c37.h5")
+        model = tf.keras.models.load_model(model_path)
     return model
